1aec8345e137a3dfaf6a2be8baa564f3bf5357e9
Alexey Kovalov <alexey.kovalov@cloudbear.ru>
2018-03-15 14:11:38 +0300

    Add room settings

--- loc/views.py
+++ loc/views.py
@@ -1,13 +1,7 @@
-from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
 from django.http import HttpResponse
 from trilateration import trelaterate, get_distance
-
-from qsstats import QuerySetStats
-
-
 from .models import Unit
-
 from django.views.generic import TemplateView
 
 
@@ -39,15 +33,15 @@ def vote(request):
         data = [list(map(int, i.split(','))) for i in str_data.split(';')]
 
     # get distance from beacons
-    dists_raw = [0]*len(data)
-    beacs = [0]*len(data)
+    dists_raw = [0] * len(data)
+    beacs = [0] * len(data)
     for i in range(len(data)):
         dists_raw[i] = get_distance(data[i][1], data[i][2])
         beacs[i] = data[i][0]
 
     indces = sorted(range(len(dists_raw)), key=lambda k: dists_raw[k])
 
-    dists_3_best = [0]*3
+    dists_3_best = [0] * 3
     for i in range(len(dists_3_best)):
         j = indces[i]
         dists_3_best[i] = (beacs[j], dists_raw[j])
@@ -58,13 +52,13 @@ def vote(request):
 
     return HttpResponse(status=201)
 
+
 @csrf_exempt
 def coords(request):
-    s=''
+    s = ''
     if request.method == 'GET':
         # Beacons table
         with open('data', 'r') as f:
             s = f.readline()
 
     return HttpResponse(s, content_type="text/plain", status=200)
-    print(s)
