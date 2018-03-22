a0d7d9a4cf946ea46dc5bd2c553ffa6e49a3d653
Alexey Kovalov <alexey.kovalov@cloudbear.ru>
2018-03-22 14:01:24 +0300

    New trelateration method

--- new_tr.py
+++ new_tr.py
@@ -1,5 +1,6 @@
 from room import get_beacons
 
+
 def trelaterate(dists_3_best):
     beacons = get_beacons()
     ap = beacons[dists_3_best[0][0]]
@@ -39,36 +40,3 @@ def trelaterate(dists_3_best):
     # p4b = vector_subtract(a, vector_multiply(ez, z))
 
     return a
-
-def trelaterate_old(dists_3_best):
-
-    beacons = get_beacons()
-    ax, ay = beacons[dists_3_best[0][0]]
-    bx, by = beacons[dists_3_best[1][0]]
-    cx, cy = beacons[dists_3_best[2][0]]
-
-    # dists_3_best = [(0, 2),(1, 3),(2, 2)]
-    ra, rb, rc = dists_3_best[0][1], dists_3_best[1][1], dists_3_best[2][1]
-
-    S = (cx**2 - bx**2 + cy**2 - by**2 + rb**2 - rc**2) / 2
-
-    T = (ax**2 - bx**2 + ay**2 - by**2 + rb**2 - ra**2) / 2
-    y = ((T * (bx - cx)) - (S * (bx - ax))) / (((ay - by) * (bx - cx)) - ((cy - by) * (bx - ax)))
-    x = ((y * (ay - by)) - T) / (bx - ax)
-
-    print(x, y, )
-
-    return x, y
-
-def get_distance(txPower, rssi):
-
-    if rssi == 0:
-        return 0
-
-    ratio = rssi * 1.0 / txPower
-    if ratio < 1:
-        return ratio**10
-    else:
-        accuracy = 0.89976 * ratio**7.7095 + 0.111
-
-    return accuracy
---
+++ test_tret.py
@@ -0,0 +1,7 @@
+from trilateration import trelaterate as tr
+from new_tr import trelaterate as tr_n
+
+dists_3_best = [(1717, 3), (303,3), (317,3)]
+
+# print(tr(dists_3_best))
+print(tr_n(dists_3_best))
